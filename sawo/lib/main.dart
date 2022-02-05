6716eb27a58c8f3513d893ab04218fd898967ed6
mousom <latifulmousom@gmail..com>
2022-02-05 14:11:49 +0530

    Upgraded SDK

---
+++ sawo/lib/main.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:sawo_sdk/sawo_sdk.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: 'Sawo Login Example',
+    home: HomeScreen(),
+  ));
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Sawo login example'),
+      ),
+      body: Center(child: SelectionButton()),
+    );
+  }
+}
+
+class SelectionButton extends StatefulWidget {
+  @override
+  _SelectionButtonState createState() => _SelectionButtonState();
+}
+
+class _SelectionButtonState extends State<SelectionButton> {
+  // sawo object for Android/ios
+  Sawo sawo = Sawo(
+    apiKey: "",
+    secretKey: "",
+  );
+
+  // user payload
+  String user = "";
+
+  void payloadCallback(context, payload) {
+    if (payload == null || (payload is String && payload.isEmpty)) {
+      payload = "Login Failed :(";
+    }
+    setState(() {
+      user = payload;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("UserData :- $user"),
+            ElevatedButton(
+              onPressed: () {
+                sawo.signIn(
+                  context: context,
+                  identifierType: 'email',
+                  callback: payloadCallback,
+                );
+              },
+              child: const Text('Email Login'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                sawo.signIn(
+                  context: context,
+                  identifierType: 'phone_number_sms',
+                  callback: payloadCallback,
+                );
+              },
+              child: const Text('Phone Login'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
